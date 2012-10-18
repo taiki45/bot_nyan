245cf95cca815b2c0b3eda8c2ae66a3899d02b17
Taiki ONO <taiks.4559@gmail.com>
2012-10-19 00:04:20 +0900

    add spec_helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+$LOAD_PATH.unshift File.expand_path("../", __FILE__)
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+
+require 'bot_nyan'
+
+def fixture_path
+  File.expand_path('../fixture/', __FILE__)
+end
+
